169d9e01bbf5f8ffd417d576e429f1e1e5cd1d76
Dayana <dayanapinela355@gmail.com>
2023-06-12 20:22:14 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'leccion_pinela_vargas',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
